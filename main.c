b6555425528d3485876f19fa5e13e88cf573dffb
KhemIngkapat <60930364+KhemIngkapat@users.noreply.github.com>
2023-12-01 13:53:34 +0700

    organizing file and add display product function

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
+#include "ctype.h"
 #include "stdio.h"
 #include "stdlib.h"
 #include "string.h"
-#include "ctype.h"
 
 typedef struct item {
     char name[20];
@@ -14,42 +14,25 @@ typedef struct item {
 static int item_count = 0;
 
 char *strtrim(char *str);
-void create_item(Item *item, char *name, double price, char *exp,int id);
-Item *sort_stock(Item *stock,int size);
-void add_to_stock(Item item,int *item_count);
+void create_item(Item *item, char *name, double price, char *exp, int id);
+Item *read_product(char *file_name);
+Item *sort_stock(Item *stock, int size);
+void add_to_stock(Item item);
 void remove_from_stock(Item item);
-void apply_promotion(Item stock[],int stock_size,double discount);
-void create_stock_file(Item *item,int size);
+void apply_promotion(Item stock[], int stock_size, double discount);
+void create_stock_file(Item *item, int size);
+void display_item(Item *stock);
 
 int main() {
-    char name[10],exp[10];
-    double price;
-    Item *stock = malloc(sizeof(Item) * 0);
-    FILE *file = fopen("final/product.csv", "r");
-    char buffer[255];
-    int size = 0;
-    while(fgets(buffer,255,file) != NULL){
-        item_count++;
-        Item item;
-        sscanf(buffer, "%20[^,],%lf,%s", name, &price, exp);
-        create_item(&item, name, price, exp,item_count);
-        size++;
-        // dynamically allocating memeory size by stock size;
-        stock = realloc(stock, sizeof(Item) *size);
-        stock[size-1] = item;
-    }
-    fclose(file);
-    sort_stock(stock,size);
-    create_stock_file(stock,size);
+    Item *stock = read_product("product.csv");
 
+    stock = sort_stock(stock, item_count);
+    create_stock_file(stock, item_count);
+    Item chocolate = {"chocolate",22.5,"6/15/2008",item_count};
+    add_to_stock(chocolate);
 
-    for (int i = 0; i < size; i++) {
-        printf("%s\n", stock[i].name);
-        printf("\t%lf", stock[i].price);
-        printf("\t%s", stock[i].exp);
-        printf("\t%ld", stock[i].timestamp);
-        printf("\t%d\n", stock[i].id);
-    }
+    display_item(stock);
+
 
     // display all item
     // recursively ask customer what they want to pick
@@ -57,7 +40,6 @@ int main() {
     // check out -> add all item price total
     // randomly pick number and apply promotion if number meet the criteria
     // thats it
-    
 
     return 0;
 }
@@ -96,21 +78,21 @@ unsigned long date_to_day(char *date) {
 
     int days_in_month[12] = {
         31, 28 + is_leap(y), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-    
-    days += d-1;
 
-    for(int i = 0;i<m-1;i++){
+    days += d - 1;
+
+    for (int i = 0; i < m - 1; i++) {
         days += days_in_month[i];
     }
 
-    for(int i = 1970;i<y;i++){
+    for (int i = 1970; i < y; i++) {
         days += 365 + is_leap(i);
     }
 
     return days;
 }
 
-void create_item(Item *item, char *name, double price, char *exp,int id) {
+void create_item(Item *item, char *name, double price, char *exp, int id) {
 
     strcpy(item->name, strtrim(name));
     item->price = price;
@@ -119,74 +101,110 @@ void create_item(Item *item, char *name, double price, char *exp,int id) {
     item->id = id;
 }
 
+Item *read_product(char *file_name){
+    char name[10], exp[10];
+    double price;
+    Item *stock = malloc(sizeof(Item) * 0);
+    FILE *file = fopen(file_name, "r");
+    char buffer[255];
+    while (fgets(buffer, 255, file) != NULL) {
+        Item item;
+        sscanf(buffer, "%20[^,],%lf,%s", name, &price, exp);
+        create_item(&item, name, price, exp, item_count+1);
+        item_count++;
+        // dynamically allocating memeory size by stock size;
+        stock = realloc(stock, sizeof(Item) * item_count);
+        stock[item_count - 1] = item;
+    }
+    fclose(file);
+    return stock;
 
-Item* sort_stock(Item* stock,int size){
-    for(int slot=1;slot<size;slot++){
+}
+
+Item *sort_stock(Item *stock, int size) {
+    for (int slot = 1; slot < size; slot++) {
         Item val = stock[slot];
-        int test_slot = slot-1;
-        while(test_slot > -1 && stock[test_slot].timestamp > val.timestamp){
+        int test_slot = slot - 1;
+        while (test_slot > -1 && stock[test_slot].timestamp > val.timestamp) {
             stock[test_slot + 1] = stock[test_slot];
             test_slot -= 1;
         }
-        stock[test_slot+1] = val;
+        stock[test_slot + 1] = val;
     }
     return stock;
 }
 
-void add_to_stock(Item item,int *item_count){
+void add_to_stock(Item item) {
     FILE *file;
-    file = fopen("final/stock.csv","a");
+    file = fopen("stock.csv", "a");
 
-    fprintf(file, "%s,%lf,%s,%d\n",item.name,item.price,item.exp,item.id);
+    fprintf(file, "%s,%lf,%s,%d\n", item.name, item.price, item.exp, item.id);
 
     fclose(file);
     item_count++;
 }
 
-void remove_from_stock(Item item){
+void remove_from_stock(Item item) {
     FILE *file, *temp;
     char buffer[2048];
-    char name[10],exp[10];
+    char name[10], exp[10];
     double price;
     int id;
-    file = fopen("final/stock.csv","r");
-    temp = fopen("final/temp_stock.csv","w");
+    file = fopen("stock.csv", "r");
+    temp = fopen("temp_stock.csv", "w");
 
     int keep_reading = 1;
-    do{
-        fgets(buffer,2048,file);
-        sscanf(buffer, "%20[^,],%lf,%10[^,],%d", name, &price, exp,&id);
-        if(feof(file)){
+    do {
+        fgets(buffer, 2048, file);
+        sscanf(buffer, "%20[^,],%lf,%10[^,],%d", name, &price, exp, &id);
+        if (feof(file)) {
             keep_reading = 0;
-        }else if(id != item.id){
-            fputs(buffer,temp);
+        } else if (id != item.id) {
+            fputs(buffer, temp);
         }
-    }while(keep_reading);
-    
+    } while (keep_reading);
+
     fclose(file);
     fclose(temp);
 
-    remove("final/stock.csv");
-    rename("final/temp_stock.csv","final/stock.csv");
-
-
+    remove("stock.csv");
+    rename("temp_stock.csv", "final/stock.csv");
 }
 
-void apply_promotion(Item stock[],int stock_size,double discount){
-    for(int i = 0;i<stock_size;i++){
+void apply_promotion(Item stock[], int stock_size, double discount) {
+    for (int i = 0; i < stock_size; i++) {
         float discounted = (double)1 - discount;
         stock[i].price = stock[i].price * discounted;
     }
 }
 
-
-void create_stock_file(Item *item,int size){
+void create_stock_file(Item *item, int size) {
     FILE *file;
-    file = fopen("final/stock.csv","w");
+    file = fopen("stock.csv", "w");
 
-    for(int i = 0;i<size;i++){
-        fprintf(file,"%s,%lf,%s,%d\n",item[i].name,item[i].price,item[i].exp,item[i].id);
+    for (int i = 0; i < size; i++) {
+        fprintf(file, "%s,%lf,%s,%d\n", item[i].name, item[i].price,
+                item[i].exp, item[i].id);
     }
     fclose(file);
+}
 
+void print_line(int length){
+    for(int i = 0;i<length;i++){
+        printf("-");
+    }
+    puts("");
+}
+
+void display_item(Item *stock){
+    puts("Hello, Welcome to our snack shop");
+    puts("this is our products");
+    print_line(30);
+
+    printf("|%-18s|%9s|\n","product","price");
+    print_line(30);
+    for(int i = 0;i<item_count;i++){
+        printf("|%-18s|%9.2lf|\n",stock[i].name,stock[i].price);
+    }
+    print_line(30);
 }
